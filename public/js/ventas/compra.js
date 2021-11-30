59abb52eb7de29173b3df540d6cc16a56ff54fd6
Tomas Lopez <tomasgit@MacBook-Air-de-Tomas.local>
2021-11-30 09:23:59 -0300

    carrito js

--- public/js/ventas/compra.js
+++ public/js/ventas/compra.js
@@ -1,14 +1,8 @@
 const eliminacion = document.querySelectorAll(".eliminar");
-const rows =document.getElementsByTagName("table tbody tr")
+const cantidad = document.querySelector(".cantidad");
+const formAgregar =document.querySelector(".formAgregar")
 
 
-
-
-// boton.addEventListener("click", (e) => {
-//     const row = rows.find("td:eq(3)").text();
-
-//     alert(row);
-// })
 
 
 
@@ -17,6 +11,43 @@ eliminacion.forEach((boton) => {
       event.preventDefault()
     //   document.querySelector("span").innerText = ` id: ${boton.id}`
 
-      alert("id:" + boton.id)
+    cantidad.style.opacity = 1;
+
+    localStorage.setItem("id", boton.id)
+    //   alert("id:" + boton.id)
     })
+  })
+
+
+  formAgregar.addEventListener("submit", (event) => {
+
+      event.preventDefault();
+
+      const idProducto = localStorage.getItem("id");
+
+      const forData = {idProducto};
+
+      for(let el of formAgregar.elements){
+          if(el.name.length > 0)
+              forData[el.name] = el.value;    
+          } 
+
+
+    //agregar a carrito para despues comprar con el metodo POST
+            
+      console.log(forData);
+
+
+      cantidad.style.opacity = 0;
+
+
+  })
+
+
+  const carrito = document.querySelector(".carrito");
+
+
+
+  carrito.addEventListener("click", () => {
+    window.location = "/public/page/roles/admin/ventas/carrito.html"
   })
