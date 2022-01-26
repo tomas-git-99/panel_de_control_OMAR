ec677327e1ef09dc957f98e16ca42152e0f5d2d9
Tomas Lopez <victor.2012@live.com.ar>
2022-01-26 13:00:49 -0300

    boton para eliminar ordenes

--- controllers/ventas/orden_detalle.ts
+++ controllers/ventas/orden_detalle.ts
@@ -20,20 +20,192 @@ export const modificarOrden = async (req: Request, res: Response) => {
         const talles = await Talle.findAndCountAll({where:{id_producto:ordenDetalle?.id_producto}});
         let productos_sin_stock:any = [];
 
-
-
-        //ACA VERIFICAMOS SI CURVA
+        
+        
+        
+        //ACA SI LO QUIERE MODIFICAR A CURVO
         if(talle == null) {
             
             //VERIFICAR SI ES POR TALLE O TOTAL
             if(productos?.cantidad == null) {
+                
+                
+                let cantidadCurva = cantidad * talles.count;
+                
+                //VERIFICAMOS SI EL ORDEN ANTERIOR ERA CURVO O POR TALLE
+                let talleCurvaoTalle:any = ordenDetalle?.talle;
+
+                //ENTRAN SI ES TALLE UNICO EL ANTERIOR DATO
+                if(talleCurvaoTalle.split(',').length == 1) {
 
 
-                let cantidadCurva = cantidad * talles.count
+
+                    let tallesDescontar:any = []
+                    let tallesDb:any = []
+                    for( let t of talles.rows) {
+
+
+                        tallesDb = [...tallesDb, { talle:t.talle, cantidad:t.cantidad}]
+                        if(ordenDetalle?.talle == t.talle){
+
+                        
+
+                            if(ordenDetalle?.cantidad > cantidad){
+
+                                //aca suma a la talle que falta
+                                let nuevaCantidad = ordenDetalle?.cantidad - cantidad;
+                                let nuevaCantidadTalle = t.cantidad + nuevaCantidad;
+
+                                tallesDescontar = [...tallesDescontar, { talle:t.talle, cantidad:nuevaCantidadTalle}]
+
+
+                            }else {
+                                //aca restamos el talle que falta
+                                let nuevaCantidad = cantidad - ordenDetalle?.cantidad;
+
+                      
+
+                                if(t.cantidad < nuevaCantidad || t.cantidad == 0){
+
+                                    productos_sin_stock.push(`El producto: "${productos?.nombre} y talle: ${t.talle}" con stock de actual: ${t.cantidad}, no tiene stock suficiente para hacer esta modificacion, cantidad que quieres descontar: ${nuevaCantidad} `);
+                                  /*   productos_sin_stock = [ ...productos_sin_stock, "jojo"] */
+                                }
+
+                        
+                                let nuevaCantidadTalle = t.cantidad - nuevaCantidad;
+
+                                tallesDescontar = [...tallesDescontar, { talle:t.talle, cantidad:nuevaCantidadTalle}]
+
+                
+
+                            }
+
+
+
+                        } else{
+
+                      
+                            if(t.cantidad < cantidad || t.cantidad == 0){
+                               
+                                productos_sin_stock.push(`El producto: "${productos?.nombre} y talle: ${t.talle}" con stock de actual: ${t.cantidad}, no tiene stock suficiente para hacer esta modificacion, cantidad que quieres descontar: ${cantidad} `);
+                                
+                                
+                            }
+
+                                
+                            let nuevaCantidad = t.cantidad - cantidad;
+                        
+
+                            tallesDescontar = [...tallesDescontar, { talle:t.talle, cantidad:nuevaCantidad}]
+                                                        
+                        }
+
+                    
+
+                        
+                    }
+                    
+                    //ACA VAMOS DESCONTAR TODO DE UNA, PARA AHORRAR EL PROBLEMA DE FALTA DE STOCK EN CADA TALLE
+                    if(productos_sin_stock.length > 0){
+                        return res.json({
+                            ok: false,
+                            error:2,
+                            msg: "No ahi stock suficiente con los productos ...",
+                            productos_sin_stock
+                        })
+                    } 
+/* 
+                    console.log(cantidad + " cantidad para descontar  o sumar")
+                    console.log(ordenDetalle?.cantidad+ " cantidad anterior")
+                    
+
+                    console.log(tallesDescontar)
+                    console.log(tallesDb) */
+
+                    for (let t of talles.rows){
+
+                        for( let d of tallesDescontar){
+
+                            if(d.talle == t.talle){
+
+
+                                //aca hacemo el update de las nuevas cantidades
+                            }
+                        }
+                    }
+
+
+
+
+                }else if ( talleCurvaoTalle.split(',').length > 1){ // ACA ENTRAR SI ES CURVO EL ANTERIOR DATO
+
+                    
+                    let cantidadDescontarPorTalle = ordenDetalle!.cantidad / talles.count;
+
+                    let tallesDescontar:any = []
+
+                    if(cantidadDescontarPorTalle > cantidad){
+    
+                        let newCantidadCurva = cantidadDescontarPorTalle - cantidad;
+    
+                    
+    
+                        // en esta parte dentri que descontar solo lo que esta en "newCantidadCurva" nada mas
+    
+                        for( let t of talles.rows) {
+    
+                            let nuevoCantidadTalle = t.cantidad + newCantidadCurva;
+                         
+                            //ahora solo tenes que hacer update a talle correspondiente
+        
+                        }
+
+
+    
+        
+    
+                    }else{
+    
+                        let newCantidadCurvaMayor = cantidad - cantidadDescontarPorTalle; // la cantidad es 10 por talle
+    
+                        
+                        
+                      
+                        for( let t of talles.rows) {
+                           
+
+                            if(t.cantidad < newCantidadCurvaMayor || t.cantidad == 0){
+                                productos_sin_stock.push(`El producto: "${productos?.nombre} y talle: ${t.talle}" con stock de actual: ${t.cantidad}, cantidad que quieres colocar: ${newCantidadCurvaMayor} ` );
+                                
+                            }
+                            console.log(t.cantidad)
+
+                            
+                            let nuevoCantidadTalle = t.cantidad - newCantidadCurvaMayor;
+                            tallesDescontar = [...tallesDescontar, { talle:t.talle, cantidad:nuevoCantidadTalle}];
+
+                        }
+
+                        if(productos_sin_stock.length > 0){
+                            return res.json({
+                                ok: false,
+                                error:2,
+                                msg: "No ahi stock suficiente con los productos ...",
+                                productos_sin_stock
+                            })
+                        } 
+
+                
+                        //aca hacer el update de las nuevas cantidades
+    
+                        
+                    }
+    
+                }
 
 
                 //VERIFICAR BIEN SI ES NECESARIO PONER ESTO ACA O DEBERIA PONERLO ABAJO DONDE CALCULAMOS LAS CANTIDADES QUE SE VAN DESCONTAR CON LA SUMAS HECHAS !!!!
-                for( let t of talles.rows) {
+/*                 for( let t of talles.rows) {
 
                     if(t.cantidad < cantidad || t.cantidad == 0){
 
@@ -49,69 +221,56 @@ export const modificarOrden = async (req: Request, res: Response) => {
                         msg: "No ahi stock suficiente con los productos ...",
                         productos_sin_stock
                     })
-                }
+                } */
 
                 //VERIFICAR BIEN SI ES NECESARIO PONER ESTO ACA O DEBERIA PONERLO ABAJO DONDE CALCULAMOS LAS CANTIDADES QUE SE VAN DESCONTAR CON LA SUMAS HECHAS !!!! FINAL
 
-                let cantidadDescontarPorTalle = ordenDetalle!.cantidad / talles.count;
-
-
-                if(cantidadDescontarPorTalle > cantidad){
-
-                    let newCantidadCurva = cantidadDescontarPorTalle - cantidad;
-
-                
-
-                    // en esta parte dentri que descontar solo lo que esta en "newCantidadCurva" nada mas
-
-                    for( let t of talles.rows) {
-
-                        let nuevoCantidadTalle = t.cantidad + newCantidadCurva;
-                     
-
-                        //ahora solo tenes que hacer update a talle correspospondiente
-
-                    }
-
-    
-
-                }else{
-
-                    let newCantidadCurvaMayor = cantidad - cantidadDescontarPorTalle;
-
-
-                    for( let t of talles.rows) {
-
-                        let nuevoCantidadTalle = t.cantidad - newCantidadCurvaMayor;
-
-                        //ahora solo tenes que hacer update a talle correspospondiente
-
-
-
-                    }
-
-                    // en esta parte dentri que sumar solo lo que esta en "newCantidadCurvaMayor" nada mas
-
-                    console.log(newCantidadCurvaMayor)
-
-                }
-
+            
  
 
 
 
+            }else{
 
-                res.json({
-                    msg:cantidadCurva
-                })
+                
+                let largoDeTalle:any = ordenDetalle?.talle;
+                
+                if(largoDeTalle.split(',').length == 1) {
+                    
+                    
+                }else if(largoDeTalle.split(',').length > 1){
+                    
+
+                    let cantidadAntigua = ordenDetalle!.cantidad / largoDeTalle.split(',').length;
+
+                   
+                    if(cantidadAntigua > cantidad){
+                        console.log("sumar")
+                    }else{
+                        let curvaNueva = cantidad - cantidadAntigua
+
+                        console.log(curvaNueva)
+                        console.log("descontar")
+                    }
+
+                }
+
+
+
             }
-
+            
         }
-
+        
+        res.json({
+            ordenDetalle
+        })
 
         
     } catch (error) {
-        
+        res.json({
+            ok: false,
+            msg: error
+        })
     }
 
 }
