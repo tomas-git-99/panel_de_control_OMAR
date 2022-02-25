64c09409a969c9cb4c190c71d5d8ca3c99e78440
Tomas Lopez <tomasgit@MacBook-Air-de-Tomas.local>
2022-02-25 07:46:54 -0300

    ultima actualizacion

--- dist/routers/ventas/carrito.js
+++ dist/routers/ventas/carrito.js
@@ -20,7 +20,7 @@ router.get("/mostrar/:id", carrito_1.mostrarCantidad_Actual_Carrito);
 router.put("/:id/:id_orden", carrito_1.descontarPorUnidad);
 /////////////// QUITAR STOCK EN EL TOTAL DEL PRODUCTO ////////////////////
 router.put("/total/:id/:id_orden", carrito_1.descontarElTotal);
-//router.put("/confirmar/compra/:id/:id_orden", pruebaParaDescontar) /// estas el prueba
-router.put("/confirmar/compra/:id/:id_orden", carrito_1.nuevaFuncionParaDescontar); /// estas el prueba
+router.put("/confirmar/compra/:id/:id_orden", carrito_1.pruebaParaDescontar); /// estas es para pruccion
+//router.put("/confirmar/compra/:id/:id_orden", nuevaFuncionParaDescontar) /// estas el prueba
 exports.default = router;
 //# sourceMappingURL=carrito.js.map
